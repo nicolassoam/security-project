f324c167e77967fbc66f9eeaff809d727844eb06
nicolassoam <nsm4456@gmail.com>
2024-12-10 15:30:25 -0300

    chaing project structure

--- config.h
+++
@@ -1,3 +0,0 @@
-#define PROJECT_DIR "C:/Users/Nicolas/Desktop/security-project"
-#define OUTPUT_DIR "C:/Users/Nicolas/Desktop/security-project/output"
-#define INPUT_DIR "C:/Users/Nicolas/Desktop/security-project/input"    
---
+++ include/config.h
@@ -0,0 +1,3 @@
+#define PROJECT_DIR "/c/Users/nsm44/Desktop/security-project"
+#define OUTPUT_DIR "/c/Users/nsm44/Desktop/security-project/output"
+#define INPUT_DIR "/c/Users/nsm44/Desktop/security-project/input"    
